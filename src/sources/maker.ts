2680c5cba288c1e3ae9ecd16f407dc27ff041533
gmbronco <83549293+gmbronco@users.noreply.github.com>
2024-04-23 13:12:51 +0200

    sDAI optimism

--- src/sources/maker.ts
+++ src/sources/maker.ts
@@ -10,6 +10,8 @@ const client = createPublicClient({
 const potAddress = '0x197e90f9fad81970ba7976f33cbd77088e5d7cf7'
 const sDAI = '0x83f20f44975d03b1b09e64809b757c47f942beea'
 const sDAIBase = '0x99ac4484e8a1dbd6a185380b3a811913ac884d87'
+const sDAIOP = '0x2218a117083f5b482b0bb821d27056ba9c04b1d3'
+
 export const maker = async () => {
   try {
     const res = await client.multicall({
@@ -25,7 +27,7 @@ export const maker = async () => {
     const apr = Math.round(
       (Number(dsr) * 10 ** -27 - 1) * 365 * 24 * 60 * 60 * 10000,
     )
-    return { [sDAI]: apr, [sDAIBase]: apr }
+    return { [sDAI]: apr, [sDAIBase]: apr, [sDAIOP]: apr }
   } catch (error) {
     console.log(error)
     return {}
