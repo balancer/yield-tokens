af9d162d01d67adcdaab87c797bebbe7260335f3
gmbronco <83549293+gmbronco@users.noreply.github.com>
2024-06-11 16:24:18 +0300

    add new stakewise address

--- src/sources/stakewise.ts
+++ src/sources/stakewise.ts
@@ -1,4 +1,5 @@
-const url = 'https://mainnet-graph.stakewise.io/subgraphs/name/stakewise/stakewise'
+const url =
+  'https://mainnet-graph.stakewise.io/subgraphs/name/stakewise/stakewise'
 
 const query = `
   {
@@ -9,7 +10,7 @@ const query = `
 `
 
 const requestQuery = {
-  query
+  query,
 }
 
 interface Response {
@@ -24,16 +25,21 @@ export const stakewise = async () => {
   const response = await fetch(url, {
     method: 'POST',
     headers: {
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
     },
-    body: JSON.stringify(requestQuery)
+    body: JSON.stringify(requestQuery),
   })
 
   const {
-    data: { osTokens: [{ apy }] },
-  } = await response.json() as Response
+    data: {
+      osTokens: [{ apy }],
+    },
+  } = (await response.json()) as Response
+
+  const apr = Math.round(Number(apy) * 100)
 
   return {
-    '0xf1c9acdc66974dfb6decb12aa385b9cd01190e38': Math.round(Number(apy) * 100),
+    '0xf1c9acdc66974dfb6decb12aa385b9cd01190e38': apr,
+    '0xf7d4e7273e5015c96728a6b02f31c505ee184603': apr,
   }
 }
