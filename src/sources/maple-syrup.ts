129d2fcdbf8c988ab51ffd1cd85ec69de28372c4
gmbronco <83549293+gmbronco@users.noreply.github.com>
2024-06-26 14:19:57 +0200

    maple syrup fixes

--- src/sources/maple-syrup.ts
+++ src/sources/maple-syrup.ts
@@ -1,37 +1,35 @@
-const url = 'https://studio.apollographql.com/public/maple-api/variant/mainnet/explorer'
+const url = 'https://api.maple.finance/v2/graphql'
 
-const query = `
-{
-    syrupApy
-  }
-`
+const query = `{
+  syrupApy
+}`
 
 const requestQuery = {
-  query
+  query,
 }
 
 interface Response {
-    data: {
-      syrupApy: string
-    }
+  data: {
+    syrupApy: string
   }
-  
-  export const maple = async () => {
-    const response = await fetch(url, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(requestQuery)
-    })
-  
-    const {
-      data: { syrupApy },
-    } = await response.json() as Response
-  
-    const actualAPR = Number(syrupApy)
-  
-    return {
-      '0x80ac24aa929eaf5013f6436cda2a7ba190f5cc0b': Math.round(actualAPR),
-    }
+}
+
+export const mapleSyrup = async () => {
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(requestQuery),
+  })
+
+  const {
+    data: { syrupApy },
+  } = (await response.json()) as Response
+
+  const actualAPR = parseFloat(syrupApy) / 1e26
+
+  return {
+    '0x80ac24aa929eaf5013f6436cda2a7ba190f5cc0b': Math.round(actualAPR),
   }
+}
